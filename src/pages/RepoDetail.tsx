1e462ecf7f1bd0029c5095ddc61abc5a73993d0e
Onos <onosejoor14@gmail.com>
2025-07-01 15:34:47 +0100

    fix: fix overflow in RepoDetails criticality tab

--- src/pages/RepoDetail.tsx
+++ src/pages/RepoDetail.tsx
@@ -492,7 +492,7 @@ const RepoDetail: React.FC<{ repoData: RepoReport }> = ({ repoData }) => {
                   </div>
                   <div className="w-full bg-gray-200 dark:bg-gray-600 rounded-full h-2">
                     <div
-                      className="bg-green-500 h-2 rounded-full"
+                      className="bg-green-500 h-2 max-w-full rounded-full"
                       style={{
                         width: `${
                           repoData.criticality.issue_comment_frequency * 100
